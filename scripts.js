9d3190601f819505cc2f3afa1202a078d8b775d7
henry <160744732+Hanwiiii@users.noreply.github.com>
2024-09-27 23:47:41 -0700

    add event listener for trail effects

--- scripts.js
+++ scripts.js
@@ -3,9 +3,15 @@ const container = document.querySelector("#container");
 function newDiv() {
     for (let i = 0; i < 256; i++) {
         let createDiv = document.createElement("div");
-        createDiv.style.backgroundColor = "lightblue";
+        createDiv.style.backgroundColor = "lightyellow";
         createDiv.className = "newDivs";
         container.appendChild(createDiv);
+        createDiv.addEventListener("mouseenter", event => {
+            event.target.style.backgroundColor = "green";
+        });
+        createDiv.addEventListener("mouseleave", event => {
+            event.target.style.backgroundColor = "lightgreen";
+        });
     }
 };
 
